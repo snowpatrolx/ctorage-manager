b2b8d0994aa3b28d1802a116ed19d48df1c1f74e
Storage Manager <user@example.com>
2026-07-07 11:20:34 +0000

    fix: add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ctorage-manager/',
   build: {
     sourcemap: 'hidden',
   },
